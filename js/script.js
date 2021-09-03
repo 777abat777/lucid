6589cdecf6818727e75d0aab57d9efcf9dab3a01
777abat777 <87979902+777abat777@users.noreply.github.com>
2021-09-03 12:16:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -128,3 +128,35 @@ const swiper = new Swiper('.swiper-container', {
    },
 });
 //Свайпер
+//Анимация элементов
+const animItems = document.querySelectorAll('.anim-items');
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll)
+   function animOnScroll() {
+      for (let index = 0; index < animItems.length; index++) {
+         const animItem = animItems[index];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('active');
+         } else {
+            animItem.classList.remove('active');
+         }
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrolltop;
+      return { top: rect.top + scrollTop || 1, left: rect.left + scrollLeft }
+   }
+   setTimeout(() => {
+      animOnScroll();
+   }, 500);
+
+}
